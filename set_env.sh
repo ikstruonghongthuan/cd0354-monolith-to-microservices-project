62f288238dfc7bd4fee45756a48b6d454d1f28dc
ikstruonghongthuan <ikstruonghongthuan@gmail.com>
2024-10-21 19:53:06 +0700

    init

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=thuanth4
+export POSTGRES_PASSWORD=123456789
+export POSTGRES_HOST=thuanth4-udagram.cjrtkgtpd1ai.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::thuanth4-udagram-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
